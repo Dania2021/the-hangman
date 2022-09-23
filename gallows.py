615b95b0853833b7dfa142251b4e425e27e2b3bf
Dania2021 <dania.khanam@gmail.com>
2022-09-23 00:43:55 +0000

    Add hangman_img function in gallows.py

---
+++ gallows.py
@@ -0,0 +1,75 @@
+"""
+Hangman images
+"""
+
+
+def hangman_img(tries):
+    """
+    Displays Hang-Hangman visuals to show man been hung on letter not in word.
+    """
+    lives_left = [
+        '''
+        --------
+        |      |
+        |      O
+        |     \|/
+        |      |
+        |     / \\
+        -
+        ''',
+        '''
+        --------
+        |      |
+        |      O
+        |     \|/
+        |      |
+        |     /
+        -
+        ''',
+        '''
+        --------
+        |      |
+        |      O
+        |     \|/
+        |      |
+        |
+        -
+        ''',
+        '''
+        --------
+        |      |
+        |      O
+        |     \|
+        |      |
+        |
+        -
+        ''',
+        '''
+        --------
+        |      |
+        |      O
+        |      |
+        |      |
+        |
+        -
+        ''',
+        '''
+        --------
+        |      |
+        |      O
+        |
+        |
+        |
+        -
+        ''',
+        '''
+        --------
+        |      |
+        |
+        |
+        |
+        |
+        -
+        ''',
+      ]
+    return lives_left[tries]
